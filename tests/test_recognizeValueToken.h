e445911e8179e156c23d04cc13bb51cc1b2ae050
brookite <brookit18@gmail.com>
2023-05-08 19:46:09 +0300

    Added tests for recognizeValueToken

--- tests/test_recognizeValueToken.h
+++ tests/test_recognizeValueToken.h
@@ -7,5 +7,14 @@ class Test_recognizeValueToken : public QObject
 {
     Q_OBJECT
 private slots:
-    
+    void integerValue();
+    void floatValue();
+    void variableValue();
+    void longVariableValue();
+    void variableWithOnlyCaptialLetterValue();
+    void variableWithCaptialLetterValue();
+    void negativeIntegerValue();
+    void negativeFloatValue();
+    void zeroFloatValue();
+    void zeroIntegerValue();
 };
